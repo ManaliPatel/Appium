9783c62305f0f813f4dde9346539fd981cdbab51
Manali Patel <manali.patel@autodesk.com>
2018-10-18 15:22:39 -0400

    setting up appium for web & iOS

---
+++ test/specs/firsttest.js
@@ -0,0 +1,12 @@
+"use strict"
+var expect = require('chai').expect;
+var assert = require('assert');
+
+describe('user logs in', function () {
+  it('creates new portfolio', function () {
+    iOS.url("http://google.com")
+    iOS.setValue('input[title="Search"]',"hello Manali")
+    iOS.keys("Enter")
+    iOS.pause(4000)
+    });
+});
